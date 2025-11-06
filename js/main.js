c26bbe3842090c6b5d4a8c35f89ece16a567a4a9
Daltongit <miltonfuel20@gmail.com>
2025-11-06 18:09:47 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const FORM_URLS = {
   es2:"https://docs.google.com/forms/d/e/1FAIpQLSfCzx3jeM2WJuBFyY3XAvF-Viim0h9G6HP1LDGbcqjOSMMhXw/viewform?usp=header",
   es3:"https://docs.google.com/forms/d/e/1FAIpQLSflGSidgSZJcrJYWTQNKbctFpVfzS3qVSlcjm9NS_2sRPORQQ/viewform?usp=header",
   es4:"https://docs.google.com/forms/d/e/1FAIpQLSfuA8SGSYIsWMt4EXqMiMpjpUvcb46Ox51-y9QUASH6I_xA0A/viewform?usp=header",
-  i1:"httpss://docs.google.com/forms/d/e/1FAIpQLSe6jzmwWf7z6r1iKkrqsJpkKTqNlvmjzMEsKOIyCIHD1fdSTg/viewform?usp=header",
+  i1:"httpshttps://docs.google.com/forms/d/e/1FAIpQLSe6jzmwWf7z6r1iKkrqsJpkKTqNlvmjzMEsKOIyCIHD1fdSTg/viewform?usp=header",
   i2:"https://docs.google.com/forms/d/e/1FAIpQLSdygMifXyF3wZ-He_X8plCIKJU8eKmU7FG6b10HbjNmBnDuYQ/viewform?usp=header",
   i3:"https://docs.google.com/forms/d/e/1FAIpQLSdJKRkm_jNDUxr3F2yjuODOuQBeSgPEx0B7HsS1lbRdJqz-tg/viewform?usp=header",
   i4:"https://docs.google.com/forms/d/e/1FAIpQLSffIHyMblXI4Yp9xp6NoSdp9HG2z1q7WHm48gabBTBZRnXrrw/viewform?usp=header",
@@ -14,12 +14,12 @@ const FORM_URLS = {
   l4:"https://docs.google.com/forms/d/e/1FAIpQLSduJY-jkte7npVrCMZiVGoHENNGJa_-RKBlS81NBy53SsaGGw/viewform?usp=header",
   m1:"https://docs.google.com/forms/d/e/1FAIpQLSffkgk_xgKKYteMgw-6aSwoJut1mXF9Zn_2kcxEsYX5HN8exg/viewform?usp=header",
   m2:"https://docs.google.com/forms/d/e/1FAIpQLSfwBsaCAUsLVtmNfIm6SLewmG-1KAJwMDFBSjfGZEpdqXo2gg/viewform?usp=header",
-  m3:"httpsia://docs.google.com/forms/d/e/1FAIpQLSfMUzEcHGcgdwjEVxKrKTkpBxfPUaWnO5zZA3_zBdxMdVI7Yw/viewform?usp=header",
+  m3:"https://docs.google.com/forms/d/e/1FAIpQLSfMUzEcHGcgdwjEVxKrKTkpBxfPUaWnO5zZA3_zBdxMdVI7Yw/viewform?usp=header",
   m4:"https://docs.google.com/forms/d/e/1FAIpQLSfjky0r3wYebeoNlYqE81vBY4FlmKWN9mD8yZ5bVtXE_oR1jA/viewform?usp=header",
   p1:"https://docs.google.com/forms/d/e/1FAIpQLScKtQOF_tJ8JCWJrrVI5xTqs9xSvMhiGHIT1MK7AMtIrlnl6Q/viewform?usp=header",
   p2:"https://docs.google.com/forms/d/e/1FAIpQLSeADNP2ISB0J4IGz92GpELqN0NXC7vAUKZps-cipgV5seEiTQ/viewform?usp=header",
   p3:"https://docs.google.com/forms/d/e/1FAIpQLScGqZ4cdszEQLta8ZbFe-H93vLloSeaKz0M-w4jUoQnJXS6eA/viewform?usp=header",
-  p4:"https://docs.google.com/forms/d/e/1FAIpQLSfqFEnDXO3RND16J0gbkSxvot1IcBjc5cE9IsGKtxqgs5Nb-w/viewform?usp=header",
+  p4:"https.google.com/forms/d/e/1FAIpQLSfqFEnDXO3RND16J0gbkSxvot1IcBjc5cE9IsGKtxqgs5Nb-w/viewform?usp=header",
   p5:"https://docs.google.com/forms/d/e/1FAIpQLScoDzv2xrlKaNcflOSjcsj47EppNgLF-ztM6oJLXMulwkf1EQ/viewform?usp=header",
   p6:"https://docs.google.com/forms/d/e/1FAIpQLSdWbpIfWK2sHtfmu-cjqgLusgax5Xb40lcOpAWSvbMgSQu81Q/viewform?usp=header",
   p7:"https://docs.google.com/forms/d/e/1FAIpQLSdk3Jdid-LbWV7rnzD54GF_Pwn7r9b7xHD9ftk1Kd5o2p6ILg/viewform?usp=header"
@@ -59,8 +59,9 @@ closeEls.forEach(el=>el.addEventListener('click', closeModal));
 modal.addEventListener('click', (e)=>{ if(e.target.classList.contains('overlay')) closeModal(); });
 document.addEventListener('keydown', (e)=>{ if(e.key==='Escape' && modal.classList.contains('show')) closeModal(); });
 
-// Lanzadores de formularios (slots)
-document.querySelectorAll('.slot').forEach(btn=>{
+// Lanzadores de formularios (Ahora .test-card)
+// --- ¡CAMBIO IMPORTANTE AQUÍ! ---
+document.querySelectorAll('.test-card').forEach(btn=>{
   btn.addEventListener('click', ()=>{
     const key = btn.getAttribute('data-test');
     const url = FORM_URLS[key];
@@ -68,7 +69,7 @@ document.querySelectorAll('.slot').forEach(btn=>{
   });
 });
 
-// Acordeones con animación de altura
+// Acordeones con animación de altura (Sin cambios, esto sigue igual)
 document.querySelectorAll('[data-acc]').forEach(acc=>{
   const btn = acc.querySelector('.header-btn');
   const panel = acc.querySelector('.panel');
